7f3d1c8eb298a44303ff1379cd0484ac29bb8653
magantus@googlemail.com <Extrem93!>
2021-02-15 14:28:58 +0100

    d

---
+++ TelegramBotSpring/src/main/java/telegramm/TelegramBotSpringApplication.java
@@ -0,0 +1,13 @@
+package telegramm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotSpringApplication.class, args);
+	}
+
+}
---
+++ TelegramBotSpring/src/test/java/telegramm/TelegramBotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package telegramm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
