5a2ad45450adb31bf2886d7927730fefe6319016
magantus@googlemail.com <Extrem93!>
2021-02-15 19:59:06 +0100

    update start

--- TelegramBotSpring/src/main/java/telegramm/bot/Bot.java
+++ TelegramBotSpring/src/main/java/telegramm/bot/Bot.java
@@ -57,15 +57,31 @@ public class Bot extends TelegramLongPollingBot {
 
 	public String switchRestToCommand(String command, CovidRequest request) {
 		switch (command) {
+		case "/start":
+		return "Es stehen folgende Befehle zur Verfügung:\r\n"
+				+ "/infection - Gibt die Anzahl der Neuinfektionen innerhalb der letzten 24 Stunden an\r\n"
+				+ "/infected - Gibt die Anzahl der aktuellen Gesamtinfektionen an\r\n"
+				+ "/increase - Gibt den prozentualen Anstieg der Infektionen an\r\n"
+				+ "/average - Gibt den durchschnittlichen Anstieg in den letzten 7 Tagen an\r\n"
+				+ "/incidencevalue - Gibt den aktuellen Inzidenzwert an\r\n"
+				+ "/incidencegoal - Gibt den Ziel-Inzidenzwert an\r\n"
+				+ "/days - Gibt die Dauer bis zum Erreichen des Ziel-Inzidenzwert an\r\n"
+				+ "/date - Gibt das Datum des Datensatzes an\r\n"
+				+ "/showallinfo - Zeigt alle Informationen an";
 		case "/showallinfo":
-			String allInfo="Datum des Datensatz: " + new JSONObject(request.getDate()).get("value")+"\n"+
-					"Es gab " + new JSONObject(request.getNewInfection()).get("value")+ " Neuinfektionen in den letzten 24 Stunden."+"\n"+
-					"Die Gesamtcoronainfektionen liegen bei " + new JSONObject(request.getTotalInfection()).get("value")+ " Menschen."+"\n"+
-					 "Der prozentuale Anstieg der letzten 24 Stunden liegt bei "+ new JSONObject(request.getPercenteInfection()).get("value") + "%."+"\n"+
-					 "Der Anstieg in den letzten 7 Tagen beträgt "+ new JSONObject(request.getAverageIncreaseDay(7)).get("value")+"\n"+
-					 "Der Inzidenzwert für Deutschland liegt aktuell bei: "+ new JSONObject(request.getRWerthTotalGermany()).get("value")+"\n"+
-					 "Der Ziel-Inzidenzwert ist" + new JSONObject(request.getTotalTargetInfection(35)).get("value") + "."+"\n"+
-					 "Es dauert aktuell" + new JSONObject(request.getTargetIncidenceForRWert(35, 7)).get("value")+ "Tage um den Ziel-Inzidenzwert zu erreichen." ;			
+			String allInfo = "Datum des Datensatz: " + new JSONObject(request.getDate()).get("value") + "\n" + "Es gab "
+					+ new JSONObject(request.getNewInfection()).get("value")
+					+ " Neuinfektionen in den letzten 24 Stunden." + "\n" + "Die Gesamtcoronainfektionen liegen bei "
+					+ new JSONObject(request.getTotalInfection()).get("value") + " Menschen." + "\n"
+					+ "Der prozentuale Anstieg der letzten 24 Stunden liegt bei "
+					+ new JSONObject(request.getPercenteInfection()).get("value") + "%." + "\n"
+					+ "Der Anstieg in den letzten 7 Tagen beträgt "
+					+ new JSONObject(request.getAverageIncreaseDay(7)).get("value") + "\n"
+					+ "Der Inzidenzwert für Deutschland liegt aktuell bei: "
+					+ new JSONObject(request.getRWerthTotalGermany()).get("value") + "\n" + "Der Ziel-Inzidenzwert ist"
+					+ new JSONObject(request.getTotalTargetInfection(35)).get("value") + "." + "\n"
+					+ "Es dauert aktuell" + new JSONObject(request.getTargetIncidenceForRWert(35, 7)).get("value")
+					+ "Tage um den Ziel-Inzidenzwert zu erreichen.";
 			return allInfo;
 		case "/date":
 			return "Datum des Datensatz: " + new JSONObject(request.getDate()).get("value");
@@ -85,52 +101,13 @@ public class Bot extends TelegramLongPollingBot {
 			return "Der Inzidenzwert für Deutschland liegt aktuell bei: "
 					+ new JSONObject(request.getRWerthTotalGermany()).get("value");
 		case "/incidencegoal":
-			return "Der Ziel-Inzidenzwert ist " + new JSONObject(request.getTotalTargetInfection(35)).get("value") + ".";
+			return "Der Ziel-Inzidenzwert ist " + new JSONObject(request.getTotalTargetInfection(35)).get("value")
+					+ ".";
 		case "/days":
 			return "Es dauert aktuell " + new JSONObject(request.getTargetIncidenceForRWert(35, 7)).get("value")
 					+ "Tage um den Ziel-Inzidenzwert zu erreichen.";
 		default:
 			return "Tut mir leid, diesen Befehl verstehe ich nicht.";
 		}
-		}
-		/*
-		case "/showallinfo":
-			String allInfo="Datum des Datensatz: " + new JSONObject(request.getDate()).get("value")+"\n"+
-					"Es gab " + new JSONObject(request.getNewInfection()).get("value")+ " Neuinfektionen in den letzten 24 Stunden."+"\n"+
-					"Die Gesamtcoronainfektionen liegen bei " + new JSONObject(request.getTotalInfection()).get("value")+ " Menschen."+"\n"+
-					 "Der prozentuale Anstieg der letzten 24 Stunden liegt bei "+ new JSONObject(request.getPercenteInfection()).get("value") + "%."+"\n"+
-					 "Der Anstieg in den letzten 7 Tagen beträgt "+ new JSONObject(request.getAverageIncreaseDay(7)).get("value")+"\n"+
-					 "Der Inzidenzwert für Deutschland liegt aktuell bei: "+ new JSONObject(request.getRWerthTotalGermany()).get("value")+"\n"+
-					 "Der Ziel-Inzidenzwert ist" + new JSONObject(request.getTotalTargetInfection(35)).get("value") + "."+"\n"+
-					 "Es dauert aktuell" + new JSONObject(request.getTargetIncidenceForRWert(35, 7)).get("value")+ "Tage um den Ziel-Inzidenzwert zu erreichen." ;			
-			return allInfo;
-		case "/date":
-			return "Datum des Datensatz: " + new JSONObject(request.getDate()).get("value");
-		case "/infection":
-			return "Es gab " + new JSONObject(request.getNewInfection()).get("value")
-					+ " Neuinfektionen in den letzten 24 Stunden.";
-		case "/infected":
-			return "Die Gesamtcoronainfektionen liegen bei " + new JSONObject(request.getTotalInfection()).get("value")
-					+ " Menschen.";
-		case "/increase":
-			return "Der prozentuale Anstieg der letzten 24 Stunden liegt bei "
-					+ new JSONObject(request.getPercenteInfection()).get("value") + "%.";
-		case "/average":
-			return "Der Anstieg in den letzten 7 Tagen beträgt "
-					+ new JSONObject(request.getAverageIncreaseDay(7)).get("value");
-		case "/incidencevalue":
-			return "Der Inzidenzwert für Deutschland liegt aktuell bei: "
-					+ new JSONObject(request.getRWerthTotalGermany()).get("value");
-		case "/incidencegoal":
-			return "Der Ziel-Inzidenzwert ist" + new JSONObject(request.getTotalTargetInfection(35)).get("value") + ".";
-		case "/days":
-			return "Es dauert aktuell" + new JSONObject(request.getTargetIncidenceForRWert(35, 7)).get("value")
-					+ "Tage um den Ziel-Inzidenzwert zu erreichen.";
-		default:
-			return "Tut mir leid, diesen Befehl verstehe ich nicht.";
-		}
-		}
-		*/
-	
-
+	}
 }
